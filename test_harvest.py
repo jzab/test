2aa8d5a6746de43dfc1e2e6f9c584b0a02e56e6b
Justin Zabilansky <jzab@mit.edu>
2017-05-05 11:22:41 -0400

    Convert all tabs to spaces

--- test_harvest.py
+++ test_harvest.py
@@ -28,26 +28,26 @@ flatFinder = FlatDetector()
 flatArea = 2000
 # capture frames from the camera
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-	# grab the raw NumPy array representing the image, the initialize the timestamp
-	# and occupied/unoccupied text
-	image = frame.array
-	lab = cv2.cvtColor(image, cv2.COLOR_BGR2Lab)
-	l,a,b = cv2.split(lab)
+    # grab the raw NumPy array representing the image, the initialize the timestamp
+    # and occupied/unoccupied text
+    image = frame.array
+    lab = cv2.cvtColor(image, cv2.COLOR_BGR2Lab)
+    l,a,b = cv2.split(lab)
 
-	# segment stable regions in the a-channel
-	# which corresponds to red-green variations within the image
-	mser = cv2.MSER_create()
-	regions = mser.detectRegions(a, None)
-	# hulls = [cv2.convexHull(p.reshape(-1,1,2)) for p in regions]
-	# cv2.polylines(image, hulls, 1, (0, 255, 0))
-	cv2.drawContours(image, regions, -1, (0, 255, 0))
+    # segment stable regions in the a-channel
+    # which corresponds to red-green variations within the image
+    mser = cv2.MSER_create()
+    regions = mser.detectRegions(a, None)
+    # hulls = [cv2.convexHull(p.reshape(-1,1,2)) for p in regions]
+    # cv2.polylines(image, hulls, 1, (0, 255, 0))
+    cv2.drawContours(image, regions, -1, (0, 255, 0))
 
-	for region in regions:
-		area, bbox = flatFinder.detect(region)
-		x, y, w, h = bbox
-		aspect = float(w)/h
-		if area >= flat_area and (1.9<=aspect<=2.1):
-			cv2.drawContours(image, [region], 0, (0, 255, 0))
+    for region in regions:
+        area, bbox = flatFinder.detect(region)
+        x, y, w, h = bbox
+        aspect = float(w)/h
+        if area >= flat_area and (1.9<=aspect<=2.1):
+            cv2.drawContours(image, [region], 0, (0, 255, 0))
             area, bbox = flatFinder.detect(region)
             x, y, w, h = bbox
             aspect = float(w)/h
@@ -57,12 +57,12 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 
 
 
-	# image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-	# ret, image = cv2.threshold(image, 20, 80,cv2.THRESH_BINARY)
+    # image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # ret, image = cv2.threshold(image, 20, 80,cv2.THRESH_BINARY)
 
-	cv2.imshow("Frame",image)
-	# time.sleep(0.5)
-	key = cv2.waitKey(1) & 0xFF
-	rawCapture.truncate(0)
-	if key == ord("q"):
-		break
+    cv2.imshow("Frame",image)
+    # time.sleep(0.5)
+    key = cv2.waitKey(1) & 0xFF
+    rawCapture.truncate(0)
+    if key == ord("q"):
+        break
