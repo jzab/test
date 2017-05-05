d38f69457fb3484d4ef1909387f54842b4c617e4
Justin Zabilansky <jzab@mit.edu>
2017-05-05 15:22:44 -0400

    backii

--- test_harvest.py
+++ test_harvest.py
@@ -65,7 +65,6 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     # cv2.drawContours(image, regions, -1, (0, 255, 0))
 
     for region in regions:
-        region = cv2.morphologyEx(dst, cv2.MORPH_OPEN, kernel)
         found, area, x, y, approx = flatFinder.detect(region)
         if found:
             cv2.drawContours(a, [region], 0, (0, 255, 0), 2)
