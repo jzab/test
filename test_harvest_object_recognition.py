c86d171b9b686d3c0043e059f04141d2ed6bf8e7
Justin Zabilansky <jzab@mit.edu>
2017-05-05 16:59:55 -0400

    lowered frame rate

--- test_harvest_object_recognition.py
+++ test_harvest_object_recognition.py
@@ -7,7 +7,7 @@ import cv2
 # initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
 camera.resolution = (640,480)
-camera.framerate = 32
+camera.framerate = 16
 rawCapture = PiRGBArray(camera, size=(640,480))
 
 # allow the camera to warmup
@@ -16,8 +16,8 @@ time.sleep(0.1)
 flatArea = 1000
 font = cv2.FONT_HERSHEY_SIMPLEX
 
-fourcc = cv2.VideoWriter_fourcc(*"XVID")
-out = cv2.VideoWriter('trial.avi', fourcc, camera.framerate, (640, 480))
+# fourcc = cv2.VideoWriter_fourcc(*"XVID")
+# out = cv2.VideoWriter('trial.avi', fourcc, camera.framerate, (640, 480))
 
 class FlatDetector:
     def __init__(self):
