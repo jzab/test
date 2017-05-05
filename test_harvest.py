117da0acb923beccdd8f8fc5354dce63db3b67f7
Justin Zabilansky <jzab@mit.edu>
2017-05-05 14:54:37 -0400

    added numpy

--- test_harvest.py
+++ test_harvest.py
@@ -2,6 +2,7 @@
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
+import numpy as np
 import cv2
 
 # initialize the camera and grab a reference to the raw camera capture
